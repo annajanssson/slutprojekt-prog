ede188472fa390b1e83728570f9664059a5c3915
anna <anna.jansson@skola.taby.se>
2024-04-15 09:35:28 +0200

    sprite
    Co-authored-by: EliasChamoun06 <EliasChamoun06@users.noreply.github.com>

--- script.js
+++ script.js
@@ -8,8 +8,8 @@ let player = {
     // name: main,
     x: centerX,
     y: centerY,
-    dx: 10, 
-    dy: 10,
+    dx: 1, 
+    dy: 1,
     direction: {
         left: false,
         right: false,
@@ -18,6 +18,16 @@ let player = {
     },
     circleSize: 20
 }
+
+bild = document.createElement("img");
+bild.src = "vapen.png";
+bild.classList.add("image");
+
+let frameIndex = 0
+const totalFrames = 2
+const spriteWidth = 100
+const spriteHeight = 100
+const scale = 1.4
 
 document.addEventListener("keydown", (e) => {
     if (e.key == "d") {
@@ -50,34 +60,59 @@ function animate() {
     requestAnimationFrame(animate)
     ctx.clearRect(0, 0, canvas.width, canvas.height );
 
-    
-    if (player.direction.right && player.x + player.circleSize < canvas.width) {
-        player.x += player.dx;
-    } else if (player.direction.left && player.x - player.circleSize > 0) {
-        player.x -= player.dx;
-    }
-    if (player.direction.down && player.y + player.circleSize < canvas.height) {
-        player.y += player.dy;
-    } else if (player.direction.up && player.y - player.circleSize > 0) {
-        player.y -= player.dy;
-    } 
-    ctx.beginPath();
-    ctx.arc(player.x, player.y, player.circleSize, 0, Math.PI * 2, true);
-    ctx.moveTo(player.x, player.y );
-    ctx.fillStyle = "white";
-    ctx.fill();
-    ctx.stroke();
 
+    if( player.direction.up == false && player.direction.down == false && player.direction.left == false && player.direction.right == false){
+        ctx.drawImage(bild, 0 * spriteWidth, 0 * spriteHeight, spriteWidth, spriteHeight, player.x, player.y,  spriteWidth * scale, spriteHeight * scale) ;
+    }
+    if (player.direction.right && player.x + 100 < canvas.width) {
+        player.x += player.dx;
+        ctx.drawImage(bild, spriteWidth, spriteHeight, spriteWidth, spriteHeight, player.x, player.y,  spriteWidth * scale, spriteHeight * scale) ;
+    } else if (player.direction.left && player.x - 100 > 0) {
+        player.x -= player.dx;
+        ctx.drawImage(bild, spriteWidth, 0 * spriteHeight, spriteWidth, spriteHeight, player.x, player.y,  spriteWidth * scale, spriteHeight * scale) ;
+    }
+    if (player.direction.down && player.y + 100 < canvas.height) {
+        player.y += player.dy;
+        ctx.drawImage(bild, 0 * spriteWidth, spriteHeight, spriteWidth,  spriteHeight, player.x, player.y,  spriteWidth * scale, spriteHeight * scale) ;
+    } else if (player.direction.up && player.y - 100 > 0) {
+        player.y -= player.dy;
+        ctx.drawImage(bild, 0 * spriteWidth, 0 * spriteHeight, spriteWidth, spriteHeight, player.x, player.y,  spriteWidth * scale, spriteHeight * scale) ;
+    } 
+
+    }
     // --------------------------------------------------------------------------------
 
- 
+    frameIndex = (frameIndex + 1) % totalFrames
+
+bild.onload = requestAnimationFrame(animate)
+
+function fiender(){
+    let enemies = {
+        x: Math.random()*canvas.width,
+        y: Math.random()*canvas.height,
+        dx: 5, 
+        dy: 5,
+        hp: 50, 
+    }
+
+    numberOfObjects = 10
+    objectlist = []
+  for (let objectNumber = 0; objectNumber < numberOfObjects; objectNumber++) {
+    
+    let object = objects[objectNumber]
+    
+    
+    
+    objectlist.push(object)
+
+    ctx.beginPath();
+    ctx.arc(500, 100, 20, 0, Math.PI * 2, true);
+    ctx.moveTo(520, 200 );
+    ctx.fillStyle = "blue";
+    ctx.fill();
+    ctx.stroke();
+    
   }
-
-  numberOfObjects = 10
-  objectlist = []
-for (let objectNumber = 0; objectNumber < numberOfObjects; objectNumber++) {
-    
-    
-}
-
+  
+}   
 animate()
